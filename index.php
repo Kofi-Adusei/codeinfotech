174b18086cef64271fe3d03b3e97fe771646063f
Adusei Asare <55342814+Kofi-Adusei@users.noreply.github.com>
2025-03-17 23:02:51 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,331 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <title>OASSMGTS Virtual School </title>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+     <link rel="stylesheet" href="dist/css/font-awesome.min.css">
+     <link rel="stylesheet" href="dist/css/owl.carousel.css">
+     <link rel="stylesheet" href="dist/css/owl.theme.default.min.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="dist/css/templatemo-style.css">
+
+</head>
+<body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+     <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
+
+               <span class="spinner-rotate"></span>
+               
+          </div>
+     </section>
+
+
+     <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO TEXT HERE -->
+                    <a href="#" class="navbar-brand">OASSMGT</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-nav-first">
+                         <li><a href="#top" class="smoothScroll">Home</a></li>
+                         <li><a href="#about" class="smoothScroll">About</a></li>
+                         <li><a href="#teachers" class="smoothScroll">Teachers</a></li>
+                         <li><a href="#laboratory" class="smoothScroll">Laboratory</a></li>
+                         <li><a href="#footer" class="smoothScroll">Contact</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="app-login-opt.php"><i ></i> <button class="btn btn-warning">Sign In</button></a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                         <li><a href="https://chat.whatsapp.com/F0XB5YSVRWYESS9UHEIzzF"><i class="fa fa-whatsapp"></i> </a></li>
+                         <li><a href="https://facebook.com"><i class="fa fa-facebook"></i> </a></li>
+                    </ul>
+               </div>
+          </div>
+     </section>
+
+
+     <!-- HOME -->
+     <section id="home">
+          <div class="row">
+                 <div class="owl-carousel owl-theme home-slider">
+                         <div class="item item-first">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Distance Learning Education Center</h1>
+                                             <h3>Our online courses are designed to fit in your industry supporting all-round with latest technologies.</h3>
+                                             <a href="#footer" class="section-btn btn btn-default smoothScroll">Discover more</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="item item-second">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Start your journey with our practical courses</h1>
+                                             <h3>Our online courses are built in partnership with technology leaders and are designed to meet industry demands.</h3>
+                                             <a href="#courses" class="section-btn btn btn-default smoothScroll">Take a course</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+
+                         <div class="item item-third">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Efficient Learning Methods</h1>
+                                             <h3>Nam eget sapien vel nibh euismod vulputate in vel nibh. Quisque eu ex eu urna venenatis sollicitudin ut at libero. Visit <a href="https://plus.google.com/+templatemo" target="_parent">templatemo</a> page.</h3>
+                                             <a href="#teachers" class="section-btn btn btn-default smoothScroll">Let's chat</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                      
+                      <div class="item item-fifth">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Efficient Learning Methods</h1>
+                                             <h3>Nam eget sapien vel nibh euismod vulputate in vel nibh. Quisque eu ex eu urna venenatis sollicitudin ut at libero. Visit <a href="https://plus.google.com/+templatemo" target="_parent">templatemo</a> page.</h3>
+                                             <a href="#laboratory" class="section-btn btn btn-default smoothScroll">Laboratory</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                        
+                    </div>
+          </div>
+     </section>
+     <!-- ABOUT -->
+     <section id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="about-info">
+                              <h2>Start your journey to a better life with online practical courses</h2>
+
+                              <figure>
+                                   <span><i class="fa fa-users"></i></span>
+                                   <figcaption>
+                                        <h3>Professional Trainers</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsa voluptatibus.</p>
+                                   </figcaption>
+                              </figure>
+
+                              <figure>
+                                   <span><i class="fa fa-certificate"></i></span>
+                                   <figcaption>
+                                        <h3>International Certifications</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsa voluptatibus.</p>
+                                   </figcaption>
+                              </figure>
+
+                              <figure>
+                                   <span><i class="fa fa-bar-chart-o"></i></span>
+                                   <figcaption>
+                                        <h3>Free for 3 months</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsa voluptatibus.</p>
+                                   </figcaption>
+                              </figure>
+                         </div>
+                    </div>
+
+                    <div class="col-md-offset-1 col-md-4 col-sm-12">
+                         <div class="entry-form">
+                             <form action="#" method="post" id="register_form" role="form" style="display: none">
+                                   <h2>Sign up</h2>
+                                   <input type="text" name="full name" class="form-control" placeholder="Full name" required="">
+                                   <input type="email" name="email" class="form-control" placeholder="Your email address" required="">
+                                   <input type="password" name="password" class="form-control" placeholder="Your password" required="">
+                                   <button class="submit-btn form-control btn-block" id="form-btn-register">Get started</button>
+                              </form>
+                             
+                             <form action="#" method="post" id="login_form" role="form" style="display: block">
+                                   <h2>Sign In</h2>
+                                   <input type="email" name="email" class="form-control" placeholder="Username" required="">
+                                   <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                                   <button type="submit" class="submit-btn form-control" id="form-btn-login">Sign</button>
+                              </form>
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+     <!-- CONTACT -->
+     <section id="teachers">
+          <div class="container">
+               <div class="row">
+                    <div class="col-md-6 col-sm-12">
+                         <form id="teachers-form" role="form" action="" method="post">
+                              <div class="section-title">
+                                   <h2>Contact us <small>we love conversations. let us talk!</small></h2>
+                              </div>
+                              <div class="col-md-12 col-sm-12">
+                                   <input type="text" class="form-control" placeholder="Enter full name" name="name" required="">
+                    
+                                   <input type="email" class="form-control" placeholder="Enter email address" name="email" required="">
+
+                                   <textarea class="form-control" rows="6" placeholder="Tell us about your message" name="message" required=""></textarea>
+                              </div>
+
+                              <div class="col-md-4 col-sm-12">
+                                   <input type="submit" class="form-control" name="send message" value="Send Message">
+                              </div>
+
+                         </form>
+                    </div>
+
+                    <div class="col-md-6 col-sm-12">
+                         <div class="contact-image">
+                             <img src="img/contact-image.jpg" class="img-responsive" alt="Smiling Two Girls">
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>       
+
+     <!--LABORATORY-->
+   
+     <!-- CONTACT -->
+     <section id="laboratory">
+               <div class="row">
+                <div class="owl-carousel owl-theme home-slider">
+                         <div class="item item-first">
+                              <div class="caption">
+                                   <div class="container">
+                                        <div class="col-md-6 col-sm-12">
+                                             <h1>Distance Learning Education Center</h1>
+                                             <h3>Our online courses are designed to fit in your industry supporting all-round with latest technologies.</h3>
+                                             <a href="#footer" class="section-btn btn btn-default smoothScroll">More Lab</a>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                </div>
+               </div>
+     </section>       
+     <!-- FOOTER -->
+     <footer id="footer">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2>Headquarter</h2>
+                              </div>
+                              <address>
+                                   <p>1800 dapibus a tortor pretium,<br> Integer nisl dui, ABC 12000</p>
+                              </address>
+
+                              <ul class="social-icon">
+                                   <li><a href="www.facebook.com" class="fa fa-facebook-square" ></a></li>
+                                   <li><a href="#" class="fa fa-twitter"></a></li>
+                                   <li><a href="#" class="fa fa-instagram"></a></li>
+                                   <li><a href="https://chat.whatsapp.com/F0XB5YSVRWYESS9UHEIzzF"><i class="fa fa-whatsapp"></i> </a></li>
+                              </ul>
+
+                              <div class="copyright-text"> 
+                                   <p>Copyright &copy; 2018 Company</p>
+                                   <p>Design: <a rel="nofollow" href="http://templatemo.com" title="html5 templates" target="_parent">Template Mo</a></p>
+                                   <p>Distribution: <a href="https://themewagon.com/">ThemeWagon</a></p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="footer-info">
+                              <div class="section-title">
+                                   <h2>Contact Info</h2>
+                              </div>
+                              <address>
+                                   <p>+233240717798</p>
+                                   <p>+233202209798</p>
+                                   <p>+233202062413</p>
+                                   <p><a href="thekofi84@gmail.com">thekofi84@gmail.com</a></p>
+                              </address>
+
+                              <div class="footer_menu">
+                                   <h2>Quick Links</h2>
+                                   <ul>
+                                        <li><a href="reg_login.php">Career</a></li>
+                                        <li><a href="#">Investor</a></li>
+                                        <li><a href="#">Terms & Conditions</a></li>
+                                        <li><a href="#">Refund Policy</a></li>
+                                   </ul>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-12">
+                         <div class="footer-info newsletter-form">
+                              <div class="section-title">
+                                   <h2>Newsletter Signup</h2>
+                              </div>
+                              <div>
+                                   <div class="form-group">
+                                        <form action="#" method="get">
+                                             <input type="email" class="form-control" placeholder="Enter your email" name="email" id="email" required="">
+                                             <input type="submit" class="form-control" name="submit" id="form-submit" value="Send me">
+                                        </form>
+                                        <span><sup>*</sup> Please note - we do not spam your email.</span>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+     <!-- SCRIPTS -->
+     <script src="dist/js/jquery.js"></script>
+     <script src="dist/js/bootstrap.min.js"></script>
+     <script src="dist/js/owl.carousel.min.js"></script>
+     <script src="dist/js/smoothscroll.js"></script>
+     <script src="dist/js/custom.js"></script>
+     <script>
+         $(document).ready(function() {
+             message="Hi am here";
+         //window.alert(message);
+         $('#form-btn-login').click(function() {
+             $('#login_form').hide();
+             $('#register_form').show();
+         });
+         $('#form-btn-register').click(function() {
+             $('#register-form').hide();
+             $('#login-form').show();
+         })
+         
+      })
+     </script>
+</body>
+</html>
